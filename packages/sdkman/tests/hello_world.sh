f7c01fec086d32870f9035faf78cef7641b611bf
Ivanq <imachug@yandex.ru>
2022-03-03 00:01:52 +0300

    Add SDKMAN package

---
+++ packages/sdkman/tests/hello_world.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+touch hello_world.jar
+touch HelloWorld.class
+if ! [ -e tmp ]; then
+	mkdir tmp
+fi
+
+run -fhello_world.java -fHelloWorld.class javac hello_world.java
+run -fHelloWorld.class -fhello_world.jar jar cf HelloWorld hello_world.jar HelloWorld.class
+diff <(run -fhello_world.jar java -jar hello_world.jar) <(echo "Hello, world!")
+
+run -fhello_world.kt -fhello_world.jar kotlinc hello_world.kt -d hello_world.jar
+diff <(run -fhello_world.jar kotlin hello_world.jar) <(echo "Hello, world!")
+
+echo scala
+run -fhello_world.scala -ftmp scalac hello_world.scala -d tmp/hello_world.jar
+diff <(run -fhello_world.jar=tmp/hello_world.jar scala hello_world.jar) <(echo "Hello, world!")
