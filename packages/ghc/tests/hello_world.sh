fc810dedb13f4cd08ba38bab959de55863971900
Ivanq <imachug@yandex.ru>
2022-03-11 19:16:41 +0300

    Reduce the size of GHC build by removing static libraries and objects

--- packages/ghc/tests/hello_world.sh
+++ packages/ghc/tests/hello_world.sh
@@ -5,5 +5,5 @@ touch hello_world
 
 run ghc --version >/dev/null
 
-run -fhello_world.hs -fhello_world ghc hello_world.hs -o hello_world
+run -fhello_world.hs -fhello_world ghc -dynamic hello_world.hs -o hello_world
 diff <(run -fhello_world ./hello_world) <(echo "Hello, world!")
